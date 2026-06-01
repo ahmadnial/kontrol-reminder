e32577644b86ca3d91a7419f74900daa88a4a782
ahmadnial <ahmadhky@gmail.com>
2026-06-02 00:11:52 +0700

    On Auto

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // Eksekusi Command WA otomatis setiap jam 08:00 pagi
-        // $schedule->command('wa:reminder-kontrol')->dailyAt('08:00');
+        $schedule->command('wa:reminder-kontrol')->dailyAt('08:00');
         // $schedule->command('wa:reminder-kontrol')->everyFiveMinutes(); // Untuk testing, jalankan setiap 5 menit
     }
 
